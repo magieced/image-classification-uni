3d05ae00e331537ae7482e70e6c3198e394af139
Sascha Conrads <sascha.conrads@outlook.de>
2026-07-12 01:42:55 +0200

    just testing differnt recource requirements

--- slurmrun_model.sh
+++ slurmrun_model.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #SBATCH --job-name=conrads-slurm-model-accuracy-testing
 #SBATCH --partition=All
-#SBATCH --mem=6G
 python3 -m venv ~/venv
 ~/venv/bin/pip install -r requirements.txt
 ~/venv/bin/python model.py
