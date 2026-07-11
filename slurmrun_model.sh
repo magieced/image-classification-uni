65191ae0203bfc555be51b5ca8928fc527dd17ff
Sascha Conrads <sascha.conrads@outlook.de>
2026-07-12 01:24:25 +0200

    capitalized sbatch occurences to SBATCH

--- slurmrun_model.sh
+++ slurmrun_model.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#sbatch --job-name=conrads-slurm-model-accuracy-testing
-#sbatch --partition=AMD
-#sbatch --mem=6G
-#sbash --gpus=1
+#SBATCH --job-name=conrads-slurm-model-accuracy-testing
+#SBATCH --partition=AMD
+#SBATCH --mem=6G
+#SBATCH --gpus=1
 python3 -m venv ~/venv
 ~/venv/bin/pip install -r requirements.txt
 ~/venv/bin/python model.py
