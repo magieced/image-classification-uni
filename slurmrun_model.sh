3e7cadcdb590e89b651ed62570c2dee51a75b8bb
Sascha Conrads <sascha.conrads@outlook.de>
2026-07-12 01:17:52 +0200

    changed job name to better reflect job contents

--- slurmrun_model.sh
+++ slurmrun_model.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-#sbatch --job-name=conrads-slurm-test
+#sbatch --job-name=conrads-slurm-model-accuracy-testing
 #sbatch --partition=AMD
 #sbatch --mem=6G
 #sbash --gpus=1
 python3 -m venv ~/venv
-python slurmtest.py
+~/venv/bin/pip install -r requirements.txt
+~/venv/bin/python model.py
