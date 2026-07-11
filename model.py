a92e88cbee996358b47753bbda022f2868a30340
Erik Hense <er.hense@gmail.com>
2026-07-11 21:49:40 +0200

    Made only one validation loader be loaded at a given time in model.py

--- model.py
+++ model.py
@@ -2,23 +2,38 @@ import model_creator
 
 # Used to create multiple models using different architectures to assess architecture accuracy
 def train_test_models():
-    model0, validation0 = model_creator.train_model(epochs=20, model_number=0,create_validation_dataloader=True)
-    model1, validation1 = model_creator.train_model(epochs=20, model_number=1,create_validation_dataloader=True)
-    model2, validation2 = model_creator.train_model(epochs=20, model_number=2,create_validation_dataloader=True)
-    model3, validation3 = model_creator.train_model(epochs=20, model_number=3,create_validation_dataloader=True)
-    model4, validation4 = model_creator.train_model(epochs=20, model_number=4,create_validation_dataloader=True)
-
-    accuracy0 = model_creator.evaluate_model(model0, validation0)
-    accuracy1 = model_creator.evaluate_model(model1, validation1)
-    accuracy2 = model_creator.evaluate_model(model2, validation2)
-    accuracy3 = model_creator.evaluate_model(model3, validation3)
-    accuracy4 = model_creator.evaluate_model(model4, validation4)
-
     file = open("accuracy_list.txt", 'w')
+
+    model0, validation0 = model_creator.train_model(epochs=20, model_number=0,create_validation_dataloader=True)
+    accuracy0 = model_creator.evaluate_model(model0, validation0)
     file.write("b0 accuracy: " + str(accuracy0) + "\n")
+    del(model0)
+    del(validation0)
+
+    model1, validation1 = model_creator.train_model(epochs=20, model_number=1,create_validation_dataloader=True)
+    accuracy1 = model_creator.evaluate_model(model1, validation1)
     file.write("b1 accuracy: " + str(accuracy1) + "\n")
+    del(model1)
+    del(validation1)
+
+    model2, validation2 = model_creator.train_model(epochs=20, model_number=2,create_validation_dataloader=True)
+    accuracy2 = model_creator.evaluate_model(model2, validation2)
     file.write("b2 accuracy: " + str(accuracy2) + "\n")
+    del(model2)
+    del(validation2)
+
+    model3, validation3 = model_creator.train_model(epochs=30, model_number=3,create_validation_dataloader=True)
+    accuracy3 = model_creator.evaluate_model(model3, validation3)
     file.write("b3 accuracy: " + str(accuracy3) + "\n")
+    del(model3)
+    del(validation3)
+
+    model4, validation4 = model_creator.train_model(epochs=40, model_number=4,create_validation_dataloader=True)
+    accuracy4 = model_creator.evaluate_model(model4, validation4)
     file.write("Simple CNN accuracy: " + str(accuracy4) + "\n")
+    del(model4)
+    del(validation4)
+
     file.close()
+    
 train_test_models()
