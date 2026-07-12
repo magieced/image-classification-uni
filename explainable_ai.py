7d2686ac8c5fa7e975799bb5ca2fdda459f3a8a2
tobias <tobi@MacBook-Air-von-tobias.local>
2026-07-12 15:01:33 +0200

    rework of occlusion sensitvity

--- explainable_ai.py
+++ explainable_ai.py
@@ -2,51 +2,69 @@ import torch
 from captum.attr import Occlusion
 import matplotlib.pyplot as plt
 import numpy as np
-from preprocessing import get_dataloaders
-from model import #model
+import preprocessing 
+import model_creator
+import torchvision.models as models
 
-training_data, validation_data = get_dataloaders(shuffled = False)
 
-#device = torch.device("mps" if torch.backends.mps.is_available() else "cpu") wurde empfohlen um das berechen zu beschleunigen 
-
-model = #model instanzieren 
-#model to device
-model.eval()#notwendig für konstante Ergbnisse 
-
-occlusion = Occlusion(model)
+data = preprocessing.get_one_dataloader(shuffled=False, image_side_length=224, augment_factor=0)
 
 heatmaps = []
 true_labels = []
 predicted_labels = []
 
-for input_tensor, i in validation_data:
-    #input_tensor= input_tensor.to(device)
-    true_class = i.item()
+def getocclusion(model_number, weigth_number, window, stride, picuture_index):
+    global heatmaps, true_labels, predicted_labels
+    heatmaps = []
+    true_labels = []
+    predicted_labels = []
+   
+    model = model_creator.load_model(model_number, weigth_number)
+    occlusion = Occlusion(model)
 
+    sliding_window_shapes1 = (3, window, window)
+    strides1 = (3, stride, stride)
+
+    data_iterator = iter(data)
+
+    for _ in range(picuture_index):
+        try: 
+            next(data_iterator)
+        except StopIteration:
+            print(f"Fehler: Index {picuture_index} ist zu groß!")
+            return
+
+    try: 
+        input_tensor, i = next(data_iterator)
+    except StopIteration:
+        print(f"Fehler: Index {picuture_index} außerhalb der Reichweite!")
+        return
+        
+    true_class = i.item()
+    
     with torch.no_grad():
         out = model(input_tensor)
-        predicted_class = torch.argmax(out, dim = 1).item()
+        predicted_class = torch.argmax(out, dim=1).item()
 
     attribute = occlusion.attribute(
         input_tensor,
-        target = predicted_class, #muss evtl noch angepasst werden für jede klasse
-        sliding_window_shapes = (3,15,15), #bestimmt die größe des abgedeckten Bereichs
-        strides = (3,8,8) #schritt weite je kleiner desto präziser bnötigt aber mehr rechenkapazität
+        target=predicted_class, 
+        sliding_window_shapes=sliding_window_shapes1, 
+        strides=strides1 
     )
 
-    heatmap = attribute.squeeze(0).detach().numpy().mean(axis= 0)
-    #heatmap = attribute.squeeze(0).cpu().detach().numpy().mean(axis= 0)#wenn über gpu
+    heatmap = attribute.squeeze(0).detach().numpy().mean(axis=0)
 
     heatmaps.append(heatmap)
     true_labels.append(true_class)
     predicted_labels.append(predicted_class)
 
-def display_picture(number):
-    picture = heatmaps[number]
-    plt.figure(figsize=(5,5))
+    picture = heatmaps[0]
+    plt.figure(figsize=(5, 5))
     plt.imshow(picture)
     plt.colorbar()
-    plt.title(f"Heatmap number: {number}")
+    plt.title(f"Heatmap number: {picuture_index}, true class: {true_class}, predicted class: {predicted_class} ")
     plt.show()
 
+    return 
 
